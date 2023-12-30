a68b5fc490514770a4c1453e8363ddaa1da50015
Sasa Nedic <svetasmak0@gmail.com>
2023-12-30 14:45:43 +0100

    Display all data on screen

--- script.js
+++ script.js
@@ -5,7 +5,15 @@ cars.forEach((data) => {
   const nameDiv = document.createElement("div");
 
   // Kreiraj i postavi HTML za svako ime
-  nameDiv.innerHTML = `<p>${data.name}</p>`;
+  nameDiv.innerHTML = `<div> <h1>${data.name}</h1>
+  <p><strong>Brand:</strong> ${data.brand}</p>
+  <p><strong>Manufactured Year:</strong> ${data.manufacturedYear}</p>
+  <p><strong>Doors:</strong> ${data.doors}</p>
+  <p><strong>Price:</strong> $${data.price}</p>
+  <p><strong>Available:</strong> ${data.available ? "Yes" : "No"}</p>
+  <hr>
+  </div>
+`;
 
   // Dodaj div sa imenom u kontejner
   car.appendChild(nameDiv);
